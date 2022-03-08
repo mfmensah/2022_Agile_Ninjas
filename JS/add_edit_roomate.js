a8acdb7bec57a06cef5c2038730031a941fd7f8d
Mark Mensah <mmensah@cvtc.edu>
2022-03-07 20:31:43 -0600

    [finishes #181336453] Add checkboxes to mark chores as complete

--- JS/add_edit_roomate.js
+++ JS/add_edit_roomate.js
@@ -3,208 +3,224 @@ let roommates = [];
 
 // Closes modal when clicked outside the box
 window.onclick = function (event) {
-  if (event.target.className === "modal") {
-    event.target.style.display = "none";
-  }
+    if (event.target.className === "modal") {
+        event.target.style.display = "none";
+    }
 };
 
 
 function sendInvitationEmail(email, name) {
-  Email.send({
-      Host: "nitroco.us",
-      Port: 25,
-      Username: "chorescheduler",
-      Password: "agileninjas",
-      To: email,
-      From: "chorescheduler@nitroco.us",
-      Subject: "Invitation to use Chore Scheduler",
-      Body: "<html><h4> Hello, <strong>" + name + "</strong ></h4 ><p>You were invited to join Chore Scheduler. Please click the link below to proceed.</p><a href=\"https://mfmensah.github.io/2022_Agile_Ninjas/index.html\">View your chores</a><br></br><em>The Agile Ninjas</em><br><br><p>Please ignore this email if you don't recognize it. Thank you.</p></html>"
-  })
-      .then(function (message) {
-          if (message === 'OK') {
-              alert("An email invitation has been sent to " + name);
-          }
-      });
-  
+    Email.send({
+        Host: "nitroco.us",
+        Port: 25,
+        Username: "chorescheduler",
+        Password: "agileninjas",
+        To: email,
+        From: "chorescheduler@nitroco.us",
+        Subject: "Invitation to use Chore Scheduler",
+        Body: "<html><h4> Hello, <strong>" + name + "</strong ></h4 ><p>You were invited to join Chore Scheduler. Please click the link below to proceed.</p><a href=\"https://mfmensah.github.io/2022_Agile_Ninjas/index.html\">View your chores</a><br></br><em>The Agile Ninjas</em><br><br><p>Please ignore this email if you don't recognize it. Thank you.</p></html>"
+    })
+        .then(function (message) {
+            if (message === 'OK') {
+                alert("An email invitation has been sent to " + name);
+            }
+        });
+
 }
 
 // Gets the current selected roommate name and return either the name or the index in the select tag box
 function getSelectedRoommateFromList(parameter) {
 
-  switch (parameter) {
-    case "name":
-      return document.getElementById('roommatesList').options[getSelectedRoommateFromList()].value;
+    switch (parameter) {
+        case "name":
+            return document.getElementById('roommatesList').options[getSelectedRoommateFromList()].value;
 
-    default:
-      return document.getElementById('roommatesList').selectedIndex;
-  }
+        default:
+            return document.getElementById('roommatesList').selectedIndex;
+    }
 }
 
 // Updates the table with number of roommates entered by the user.
 function AddOrUpdateRoomateToRows() {
-  let table = document.querySelector('.tableBody');
-  table.innerHTML = "";
-  
-  roommates.forEach(roommate => {
-    let new_tr_tag = document.createElement('tr');
-    let new_td_tag = document.createElement('td');
-    new_td_tag.className = 'roommateName';
-    new_tr_tag.id = roommate.name;
+    let table = document.querySelector('.tableBody');
+    table.innerHTML = "";
 
-    new_td_tag.appendChild(document.createTextNode(roommate.name));
-    new_tr_tag.appendChild(new_td_tag);
+    roommates.forEach(roommate => {
+        let new_tr_tag = document.createElement('tr');
+        let new_td_tag = document.createElement('td');
+        new_td_tag.className = 'roommateName';
+        new_tr_tag.id = roommate.name;
 
-    // Adds the empty tds tags to the row
-    for (let index = 0; index < 7; index++) {
-      let new_empty_td_tag = document.createElement('td');
-      new_empty_td_tag.setAttribute("class", "addUpdateChore");
-      
-      for (let index = 0; index < 5; index++) {
-        let innerTdEditableDiv = document.createElement('div');
-        innerTdEditableDiv.className = "chore-imput"
-        innerTdEditableDiv.setAttribute("contenteditable", "true");
-        new_empty_td_tag.append(innerTdEditableDiv);
-      }
+        new_td_tag.appendChild(document.createTextNode(roommate.name));
+        new_tr_tag.appendChild(new_td_tag);
 
-      new_tr_tag.append(new_empty_td_tag);
-    
+        // Adds the empty tds tags to the row
+        for (let indexTD = 0; indexTD < 7; indexTD++) {
+            let new_empty_td_tag = document.createElement('td');
+            new_empty_td_tag.setAttribute("class", "addUpdateChore");
+
+            for (let index = 0; index < 5; index++) {
+                let innerTdEditableDiv = document.createElement('div');
+                innerTdEditableDiv.className = "chore-imput"
+                innerTdEditableDiv.setAttribute("contenteditable", "true");
+                new_empty_td_tag.append(innerTdEditableDiv);
+
+                let checkBox = document.createElement("INPUT");
+                checkBox.onchange = handleCompleteCheck(indexTD, index);
+                checkBox.type = "checkbox";
+                checkBox.checked = false;
+                checkBox.setAttribute("id", indexTD + index);
+                new_empty_td_tag.append(checkBox);
+            }
+
+            new_tr_tag.append(new_empty_td_tag);
+
+        }
+
+        // Updating the table
+        table.appendChild(new_tr_tag);
+    });
+}
+
+//handles checked events for complete checkbox
+function handleCompleteCheck(day, chore) {
+    var checkbox = document.getElementById(day + chore);
+    console.log(day + "_" + chore);
+    if (checkbox !== null && checkbox.checked) {
+        alert(day + " " + chore + " complete");
     }
-
-    // Updating the table
-    table.appendChild(new_tr_tag);
-  });
 }
 
 // Updates the roommate select list on the page based on the roommates array 
 function UpdateRoommatesSelectList() {
-  let selectListBox = document.querySelector("#roommatesList");
-  
-  selectListBox.innerHTML = "";
-  
-  roommates.forEach(roommate => {
-    let addRoommateToOptionTag = new Option(roommate.name, roommate.name);
+    let selectListBox = document.querySelector("#roommatesList");
 
-    selectListBox.add(addRoommateToOptionTag, undefined);
-  });
+    selectListBox.innerHTML = "";
 
-  AddOrUpdateRoomateToRows();
+    roommates.forEach(roommate => {
+        let addRoommateToOptionTag = new Option(roommate.name, roommate.name);
+
+        selectListBox.add(addRoommateToOptionTag, undefined);
+    });
+
+    AddOrUpdateRoomateToRows();
 
 }
 
 // Adds new roommate info into a object and adds it to the global array containing the list of all current rommates
 function AddOrEditRoommate(name, email, phone) {
-  let newRoommateInfo = {
-    name: name,
-    email: email,
-    phone: phone
+    let newRoommateInfo = {
+        name: name,
+        email: email,
+        phone: phone
     };
     sendInvitationEmail(email, name);
-  roommates.push(newRoommateInfo);
+    roommates.push(newRoommateInfo);
 }
 
 // Closes the popup box/framte for adding, editing or deleting roommates 
 function CloseCurrentModal(id) {
-  document.querySelector("#"+id).style.display = "none";
+    document.querySelector("#" + id).style.display = "none";
 }
 
 function AddNewRoommate() {
 
-  // Gets values from the form and stores in local variables
-  let newRoomateName = document.querySelector("#addRoommate input[name='name']").value;
-  let newRoomateEmail = document.querySelector("#addRoommate input[name='email']").value;
-  let newRoomatePhone = document.querySelector("#addRoommate input[name='phone']").value;
+    // Gets values from the form and stores in local variables
+    let newRoomateName = document.querySelector("#addRoommate input[name='name']").value;
+    let newRoomateEmail = document.querySelector("#addRoommate input[name='email']").value;
+    let newRoomatePhone = document.querySelector("#addRoommate input[name='phone']").value;
 
-  if (newRoomateName !== "" && newRoomateEmail !== "") {
+    if (newRoomateName !== "" && newRoomateEmail !== "") {
 
-    AddOrEditRoommate(newRoomateName, newRoomateEmail, newRoomatePhone);
+        AddOrEditRoommate(newRoomateName, newRoomateEmail, newRoomatePhone);
 
-    UpdateRoommatesSelectList();
+        UpdateRoommatesSelectList();
 
-    // Close the window add new roomate window
-    CloseCurrentModal("addRoommate");
-  } else {
-    alert("Please, enter new roommate's name and valid email to add to the list.");
-  }
-  
+        // Close the window add new roomate window
+        CloseCurrentModal("addRoommate");
+    } else {
+        alert("Please, enter new roommate's name and valid email to add to the list.");
+    }
+
 }
 
 function EditSelectedRoommate() {
-  roommates.forEach(roommate => {
-    if (roommate.name === getSelectedRoommateFromList("name")) {
-      roommate.name = document.querySelector("#editRoommate input[name='name']").value;
-      roommate.email = document.querySelector("#editRoommate input[name='email']").value;
-      roommate.phone = document.querySelector("#editRoommate input[name='phone']").value;
-      UpdateRoommatesSelectList();
-    }
-  });
-  CloseCurrentModal("editRoommate");
+    roommates.forEach(roommate => {
+        if (roommate.name === getSelectedRoommateFromList("name")) {
+            roommate.name = document.querySelector("#editRoommate input[name='name']").value;
+            roommate.email = document.querySelector("#editRoommate input[name='email']").value;
+            roommate.phone = document.querySelector("#editRoommate input[name='phone']").value;
+            UpdateRoommatesSelectList();
+        }
+    });
+    CloseCurrentModal("editRoommate");
 }
 
 function DisplaySelectedRoommateInfo() {
-  
-  roommates.forEach(roommate => {
-    if (roommate.name === getSelectedRoommateFromList("name")) {
-      document.querySelector("#editRoommate input[name='name']").value = roommate.name;
-      document.querySelector("#editRoommate input[name='email']").value = roommate.email;
-      document.querySelector("#editRoommate input[name='phone']").value = roommate.phone;
-    }
-  });
+
+    roommates.forEach(roommate => {
+        if (roommate.name === getSelectedRoommateFromList("name")) {
+            document.querySelector("#editRoommate input[name='name']").value = roommate.name;
+            document.querySelector("#editRoommate input[name='email']").value = roommate.email;
+            document.querySelector("#editRoommate input[name='phone']").value = roommate.phone;
+        }
+    });
 }
 
 function DeleteSelectedRoommate() {
-  roommates.forEach((roommate, index) => {
-    if (roommate.name === getSelectedRoommateFromList("name")) {
-      delete roommates[index];
-    }
-  });
+    roommates.forEach((roommate, index) => {
+        if (roommate.name === getSelectedRoommateFromList("name")) {
+            delete roommates[index];
+        }
+    });
 
-  UpdateRoommatesSelectList();
+    UpdateRoommatesSelectList();
 
-  CloseCurrentModal("deleteRoommate");
+    CloseCurrentModal("deleteRoommate");
 }
 
 // Modal enabler to show add, edit or delete modals roommates from select tag list
 let modalBtns = [...document.querySelectorAll(".button")];
 
 modalBtns.forEach(function (btn) {
-  btn.onclick = function () {
-    let roommateSelected = getSelectedRoommateFromList();
-    let modal = btn.getAttribute("data-modal");
-    
-    if ( (modal === "deleteRoommate" || modal === "editRoommate") && roommateSelected <= "-1") {
-      alert("Please, select a roommate to be edited or deleted");
+    btn.onclick = function () {
+        let roommateSelected = getSelectedRoommateFromList();
+        let modal = btn.getAttribute("data-modal");
 
-    } else if(modal === "editRoommate") {
-      DisplaySelectedRoommateInfo();
-      document.getElementById(modal).style.display = "block";
+        if ((modal === "deleteRoommate" || modal === "editRoommate") && roommateSelected <= "-1") {
+            alert("Please, select a roommate to be edited or deleted");
 
-    } else if(modal === "deleteRoommate") {
-      let roommateToDelete = getSelectedRoommateFromList("name");
-      document.querySelector("span#roommateMarketForDelete").innerText = roommateToDelete;
-      
-      document.getElementById(modal).style.display = "block";
-    } else {
-      document.getElementById(modal).style.display = "block";
-    }
-  };
+        } else if (modal === "editRoommate") {
+            DisplaySelectedRoommateInfo();
+            document.getElementById(modal).style.display = "block";
+
+        } else if (modal === "deleteRoommate") {
+            let roommateToDelete = getSelectedRoommateFromList("name");
+            document.querySelector("span#roommateMarketForDelete").innerText = roommateToDelete;
+
+            document.getElementById(modal).style.display = "block";
+        } else {
+            document.getElementById(modal).style.display = "block";
+        }
+    };
 });
 
 let closeBtns = [...document.querySelectorAll(".close")];
 closeBtns.forEach(function (btn) {
-  btn.onclick = function () {
-    let modal = btn.closest(".modal");
-    modal.style.display = "none";
-  };
+    btn.onclick = function () {
+        let modal = btn.closest(".modal");
+        modal.style.display = "none";
+    };
 });
 
 // Highlight roommate when the name is clicked on from the roommates list
 function highlightRoommate() {
 
-  roommates.forEach(roommate => {
-    document.getElementById(roommate.name).style.backgroundColor = "#696969";
-  })
+    roommates.forEach(roommate => {
+        document.getElementById(roommate.name).style.backgroundColor = "#696969";
+    })
 
-  roommate = document.getElementById("roommatesList").value;
-  document.getElementById(roommate).style.backgroundColor = "white";
+    roommate = document.getElementById("roommatesList").value;
+    document.getElementById(roommate).style.backgroundColor = "white";
 
 }
