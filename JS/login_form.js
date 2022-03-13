20ddc23e7f9bc2ceac6a45fa8368364b832b6a4f
Bemental77 <bementcw@gmail.com>
2022-03-13 15:34:14 -0500

    Login and Logout button will change depending on the logged in/ logged out status of the user. [#181505457}

--- JS/login_form.js
+++ JS/login_form.js
@@ -4,12 +4,25 @@ function openForm() {
 
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
+    document.getElementById('loginButton').style.display = 'none';
+    document.getElementById('logoutButton').style.display = 'inline';
+
+
 
 }
 
+
 function logoutUser(){
+    document.getElementById('loginButton').style.display = 'inline';
+    document.getElementById('logoutButton').style.display = 'none';
+
+    //to change access to roommate
     document.getElementById("roommatesControls").style.display = "none";
+
+    //to remove ability to change chores
     document.getElementsByClassName("addUpdateChore").disabled = true;
+
+
 }
 
 
