6ea69d0319fbf1caf646ba13c56b838bb89e8003
Mark Mensah <mmensah@cvtc.edu>
2022-03-14 20:41:51 -0500

    [Finishes #181505452] Created text area for rules

---
+++ JS/api_helper.js
@@ -0,0 +1,2 @@
+let api_get_user = "https://nitroco.us/cs/api/users";
+let api_authenticate_user = "https://nitroco.us/cs/api/authenticate";
---
+++ JS/roommate_rules.js
@@ -0,0 +1,32 @@
+function getRules() {
+    var request_ = new XMLHttpRequest();
+    request_.open("GET", "https://nitroco.us/cs/api/Rules", true);
+    request_.send();
+    request_.onreadystatechange = function () {
+        if (request_.readyState == 4 && request_.status == 200) {
+            var response = request_.responseText;
+            var obj = JSON.parse(response);
+
+            console.log(obj);
+            document.getElementById("rules").value = obj;
+        }
+    }
+}
+
+function SetRules() {
+    var rule = document.getElementById("rules").value;
+    var request_ = new XMLHttpRequest();
+    request_.open("Post", "https://nitroco.us/cs/api/Rules?value="+rule, true);
+    request_.send();
+    request_.onreadystatechange = function () {
+        if (request_.readyState == 4 && request_.status == 200) {
+            var response = request_.responseText;
+            var obj = JSON.parse(response);
+
+            console.log(obj);
+            alert("House Rules have been updated successfully.");
+        }
+    }
+}
+
+getRules();
